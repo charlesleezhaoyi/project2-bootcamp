917f55278faf382955830cc67e8ccdc252b2ea0e
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2024-01-27 09:28:07 +0800

    removed redundant code in App.js

--- src/App.js
+++ src/App.js
@@ -312,22 +312,7 @@ const App = () => {
 
         {isLoggedIn && (
           <StyledContainer>
-            <StyledGridItem item>
-              {/* <h2>Welcome back {user.email}</h2> */}
-              {/* <AppLinks /> */}
-
-              {/* <Button
-                variant="outlined"
-                onClick={(e) => {
-                  setIsLoggedIn(false);
-                  signOut(auth);
-                  setUser({});
-                }}
-                sx={{ marginLeft: "20px" }}
-              >
-                Log out
-              </Button> */}
-            </StyledGridItem>
+            <StyledGridItem item></StyledGridItem>
             <StyledGridItem
               item
               style={{
